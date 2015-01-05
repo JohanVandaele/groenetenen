14c7ec0e5688c1442143259552bc4f6c631ea595
“JohanVandaele” <johanvandaele@hotmail.com>
2015-01-05 13:59:15 +0100

    commit

---
+++ src/test/java/be/vdab/entities/FiliaalTest.java
@@ -0,0 +1,31 @@
+package be.vdab.entities;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import be.vdab.valueobjects.Adres;
+
+public class FiliaalTest
+{
+//	private Filiaal filiaal;
+	private Filiaal filiaal = new Filiaal("FiliaalTest",false,new BigDecimal(100),new Date(), new Adres("Vlamingstraat","8",8500,"Wevelgem"));
+
+	@Before
+	public void before()
+	{
+		//Filiaal filiaal = new Filiaal("FiliaalTest",false,new BigDecimal(100),new Date(), new Adres("Vlamingstraat","8",8500,"Wevelgem"));
+	}
+	
+	@Test
+	public void getNaam()
+	{
+		//System.out.println("----------->"+filiaal.getNaam());
+		assertEquals("FiliaalTest", filiaal.getNaam());
+	}
+}
+
